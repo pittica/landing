dedfc911d303e5763311163ea0778fb750c0c058
Lucio Benini <iiriya@iiriya.com>
2022-03-02 23:42:48 +0100

    Fix: 404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -5,7 +5,14 @@ export default function NotFoundPage() {
   return (
     <section className={classNames("hero", "is-halfheight")}>
       <div className="hero-body">
-        <h1 className={classNames("title", "has-text-centered")}>404</h1>
+        <div className="container">
+          <h1 className={classNames("title", "has-text-centered")}>404</h1>
+          <div className="content">
+            <p className={classNames("subtitle", "has-text-centered")}>
+              Go to <a href="https://pittica.com">pittica.com</a>
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   )
